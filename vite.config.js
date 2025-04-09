3dfe0641fc10989ed04cf698e71268d8127a1c76
avii1253 <anandavinash1253@gmail.com>
2025-04-09 13:33:48 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss()
+  base: '/Weather_Test_App/', 
+  build: {
+    outDir: 'build',
+  },
+  plugins: [
+    react(),
+    tailwindcss()
   ],
 })
